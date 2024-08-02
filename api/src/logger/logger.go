c1658eb92753f8fb2110727a958be8a166962fe3
giovanni <giovannimarassi@gmail.com>
2024-08-02 18:21:59 -0300

    change time format logger

--- api/src/logger/logger.go
+++ api/src/logger/logger.go
@@ -7,5 +7,5 @@ import (
 )
 
 func Logger(r *http.Request, statusCode int){
-	log.Printf("\n [%s] %s %s %s %d", time.Now(), r.Method, r.RequestURI, r.Host, statusCode)
+	log.Printf("\n [%s] %s %s %s %d", time.Now().Format(time.RFC822), r.Method, r.RequestURI, r.Host, statusCode)
 }
